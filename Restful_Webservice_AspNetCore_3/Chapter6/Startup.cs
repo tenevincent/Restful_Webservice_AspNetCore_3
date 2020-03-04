f6c3bf6f92c50e8e2f88b94f80642144207e6846
vincent tene <vincent.tene@gmail.com>
2020-03-04 16:07:48 +0100

    refactoring

--- Restful_Webservice_AspNetCore_3/Chapter6/Startup.cs
+++ Restful_Webservice_AspNetCore_3/Chapter6/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SampleAPI.CustomRouting;
 using SampleAPI.Repositories;
 
 namespace SampleAPI
@@ -14,15 +16,28 @@ namespace SampleAPI
         {
             services
                 .AddSingleton<IOrderRepository, MemoryOrderRepository>()
-                .AddControllers()
-                .AddNewtonsoftJson();
-                
+                .AddControllers();
+
+
+            //1° Configure the routing constraints
+            //services.Configure<RouteOptions>(options => {
+            //    options.ConstraintMap.Add("currency", typeof(CurrencyConstraint));
+            //});
+
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
-            app.UseEndpoints(item => { item.MapControllers(); });
+            app.UseEndpoints(x => { x.MapControllers(); });
+
+            //1° Configure the routing constraints
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute("default", "{controller}/{action}/{currency}");
+            //});
+
         }
     }
 }
